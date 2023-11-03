c04aa483c2c1d841fbde3d4812372c84fed4ab65
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-11-02 23:27:50 -0600

    Ejercicio OBJ OpenGL

---
+++ OGL1/OGL1/OGL1/Renderer_OpenGL.py
@@ -0,0 +1,75 @@
+from OpenGL.GL.shaders import fragment_shader, vertex_shader
+import pygame
+from pygame.locals import * 
+from gl import Renderer
+from model import Model
+from shaders import *
+import glm
+
+width = 1920
+height = 1080
+
+pygame.init()
+
+screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
+clock = pygame.time.Clock()
+
+rend = Renderer(screen)
+
+rend.setShader(vertexShader = vertex_shader, 
+			   fragmentShader = fragment_shader)
+
+
+model = Model("modelos/hongo.obj")
+model.loadTexture("texturas/thongo.bmp")
+model.position.z = -5.5
+model.scale = glm.vec3(2,2,2)
+model.rotation.x = 45
+
+rend.scene.append(model)
+
+isRunning = True
+
+while isRunning:
+
+
+	deltaTime = clock.tick(60) / 1000
+
+	keys = pygame.key.get_pressed()
+
+	for event in pygame.event.get():
+
+		if event.type == pygame.QUIT:
+			isRunning = False
+
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				isRunning = False
+
+	if keys[K_d]:
+		rend.camPosition.x -= 5 * deltaTime
+	
+	if keys[K_a]:
+		rend.camPosition.x += 5 * deltaTime
+
+	if keys[K_w]:
+		rend.camPosition.y -= 5 * deltaTime
+	
+	if keys[K_s]:
+		rend.camPosition.y += 5 * deltaTime
+
+	if keys[K_q]:
+		rend.camPosition.z -= 5 * deltaTime
+	
+	if keys[K_e]:
+		rend.camPosition.z += 5 * deltaTime
+
+	model.rotation.y += 45 * deltaTime
+
+	rend.elapsedTime += deltaTime
+
+	rend.render()
+
+	pygame.display.flip()
+
+pygame.quit()
---
+++ OGL1/OGL1/OGL1/shaders.py
@@ -0,0 +1,37 @@
+vertex_shader = '''
+#version 450 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec2 texCoords;
+
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+uniform float time;
+
+out vec2 uvs;
+
+void main()
+{
+	vec4 newPos = vec4(position.x, position.y + sin(time)/4, position.z, 1.0);
+
+	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(newPos);
+	uvs = texCoords;
+}
+'''
+
+fragment_shader = '''
+#version 450 core
+
+layout (binding = 0) uniform sampler2D tex;
+
+in vec2 uvs;
+
+out vec4 fragmentColor;
+
+void main()
+{
+	fragmentColor = texture(tex, uvs);
+}
+'''
